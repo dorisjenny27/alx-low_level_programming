0358ec8d031becac71464bbe334f46200f32b009
dorisjenny27 <dorisjenny27@gmail.com>
2023-06-16 09:27:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - A c program that prints a line to the standard error
  * Return: 1 (Success)
  */
-int main (void)
+int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful" - Dora Korpar, 2015-10-19));
 return (1);
 }
