d22c741f0a9c096ecf7fd8b4816baca846f11313
dorisjenny27 <dorisjenny27@gmail.com>
2023-06-23 11:38:33 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers since 0 up to 9
+ * Return: The numbers since 0 up to 9
+ */
+
+void print_most_numbers(void)
+
+{
+
+	int y;
+
+	for (y = 48; y < 58; y++)
+	{
+	if (y != 50)
+	{
+	if (y != 52)
+	{
+	_putchar(y);
+	}
+	}
+	}
+	_putchar('\n');
+}
