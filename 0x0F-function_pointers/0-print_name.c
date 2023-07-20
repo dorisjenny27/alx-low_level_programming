c56b160b377d7c04def2aee6533f97fd9e3c0e7d
dorisjenny27 <dorisjenny27@gmail.com>
2023-07-20 07:06:50 -0700

    If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,16 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
-* print_name - prints a name using a function pointer
-* @name: string to add
-* @f: pointer to function
-* Return: nothing
-**/
+ * print_name: prints a name using a function pointer
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-return;
+	if (name == NULL || f == NULL)
+		return;
 
-
-f(name);
+	f(name);
 }
+
